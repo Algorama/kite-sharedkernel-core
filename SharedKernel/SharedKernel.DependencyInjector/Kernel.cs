16d29ce86f8f9501b99e31f8cc84cbbba1bfce32
Devalgo <devalgo@algorama.com.br>
2018-02-23 13:44:52 -0300

    Implementado Projeto DependencyInjector com SimpleInjector

---
+++ SharedKernel/SharedKernel.DependencyInjector/Kernel.cs
@@ -0,0 +1,52 @@
+using System;
+using SharedKernel.Domain.Repositories;
+using SharedKernel.Domain.Services;
+using SharedKernel.EntityFramework.Repositories;
+using SimpleInjector;
+
+namespace SharedKernel.DependencyInjector
+{
+    public class Kernel
+    {
+        private static Container _kernel;
+
+        public static Container GetKernel()
+        {
+            if(_kernel == null)
+                throw new Exception("Kernel não foi inicializado");
+            
+            return _kernel;
+        }
+
+        private static void StartBase()
+        {
+            _kernel = new Container();
+            _kernel.Register(typeof(IQueryService<>), typeof(QueryService<>));
+            _kernel.Register(typeof(ICrudService<>), typeof(CrudService<>));
+        }
+
+        public static void Start()
+        {
+            StartBase();
+            _kernel.Register<IHelperRepository, HelperRepository>();
+        }
+
+        public static T Get<T>() where T : class
+        {
+            if(_kernel == null)
+                throw new Exception("Kernel não foi inicializado");
+
+            return _kernel.GetInstance<T>();
+        }
+
+        public static void Bind<TFrom, TTo>() 
+            where TTo : class, TFrom
+            where TFrom : class
+        {
+            if(_kernel == null)
+                throw new Exception("Kernel não foi inicializado");
+
+            _kernel.Register<TFrom, TTo>();
+        }
+    }
+}
