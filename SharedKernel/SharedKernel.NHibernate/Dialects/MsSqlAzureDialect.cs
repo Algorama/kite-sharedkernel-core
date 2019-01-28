e0eb8a283bf36539d01ada4bd6fbd84ea082dbf0
Devalgo <devalgo@algorama.com.br>
2019-01-28 11:33:21 -0200

    Incluso Dialetos para SqlAzure e PgSql

---
+++ SharedKernel/SharedKernel.NHibernate/Dialects/MsSqlAzureDialect.cs
@@ -0,0 +1,9 @@
+﻿using NHibernate.Dialect;
+
+namespace SharedKernel.NHibernate.Dialects
+{
+    public class MsSqlAzureDialect : MsSql2008Dialect
+    {
+        public override string PrimaryKeyString => "primary key CLUSTERED";
+    }
+}
---
+++ SharedKernel/SharedKernel.NHibernate/Dialects/PgSqlCustomDialect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate.Dialect;
+using NHibernate.Id;
+using NHibernate.Type;
+
+namespace SharedKernel.NHibernate.Dialects
+{
+    public class PgSqlCustomDialect : PostgreSQLDialect
+    {
+        public override Type NativeIdentifierGeneratorClass => typeof(SequenceByConvention);
+    }
+
+    public class SequenceByConvention : SequenceGenerator
+    {
+        public override void Configure(IType type, IDictionary<string, string> parms, Dialect dialect)
+        {
+            parms["sequence"] = GetSequenceNameFromTableName(parms["target_table"]);
+            base.Configure(type, parms, dialect);
+        }
+
+        private static string GetSequenceNameFromTableName(string tableName)
+        {
+            return tableName.Substring(0, Math.Min(26, tableName.Length)) + "_seq";
+        }
+    }
+}
